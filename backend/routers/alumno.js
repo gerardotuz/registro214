697594f177dc26bf8f8c89b883ad26c82bc2f8cb
gerardotuz <82822594+gerardotuz@users.noreply.github.com>
2026-02-07 17:21:24 -0500

    Update alumno.js

--- backend/routers/alumno.js
+++ backend/routers/alumno.js
@@ -60,7 +60,7 @@ router.get('/folio/:folio', async (req, res) => {
 // ===================================
 
 async function generarFolio() {
-  const prefijo = "CBTIS272-";
+  const prefijo = "CBTIS214-";
 
   const ultimo = await Alumno.findOne({
     folio: { $regex: `^${prefijo}` }
